6ae12b61f9bfaa065c7cd23d90358cc1b75624a2
ukerem <kerem.uysal@ynov.com>
2025-10-27 15:11:04 +0100

    feature-color

--- main.go
+++ main.go
@@ -16,7 +16,13 @@ func main() {
 	})
 	
 	http.HandleFunc("/url", URLHandler)
+	http.HandleFunc("/color", ColorHandler)
 
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
+}
+
