9f891f05fb2348e23f01cf5d7bbd4309cff76805
ukerem <kerem.uysal@ynov.com>
2025-10-27 15:31:08 +0100

    fake colleague

---
+++ fake-colleague.sh
@@ -0,0 +1,7 @@
+git clone $1 /tmp/git_exam
+cd /tmp/git_exam
+git checkout feature-audio-support
+echo "func tacos(){}" >> main.go
+git add . && git commit -m "add : func tacos (to dev)" && git push
+rm -rf /tmp/git_exam
+chmod +x fake-colleague.sh
